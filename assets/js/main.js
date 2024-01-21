c40ed571f6f7e1451152811cd7d899656a70d1f7
Serlink-Innovations <serlink.business@outlook.com>
2024-01-21 11:44:05 -0400

    v1.0.0

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+const nav = document.querySelector(".nav");
+
+function toggleMobileNav() {
+    if (nav.style.display === "none") {
+        nav.style.display = "flex !important"
+    } else {
+        nav.style.display === "none"
+    }
+}
