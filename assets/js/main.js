6857541c2463e797324aeee659c8439b7b6163a8
Serlink-Innovations <serlink.business@outlook.com>
2024-01-22 06:28:17 -0400

    v1.0.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
 function toggleMobileNav(navToggle) {
     navToggle = document.querySelector(".nav");
+    removeContainer = document.querySelector(".navbar")
 
     if (navToggle.classList.contains("nav")) {
-        navToggle.classList.toggle("show")
+        navToggle.classList.toggle("show");
+        removeContainer.classList.toggle("container");
     }
 }
